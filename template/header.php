b1cdfebc9da1f778d38e3c93b31f607577bf0dcd
HOME SCHOOL <147116520+homeschool221@users.noreply.github.com>
2025-07-19 13:29:02 +0000

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,12 @@
+<div class="bg-primary position-relative">
+  <div class="overlay-gradient"></div>
+  <div class="container position-relative">
+  	<div class="row py-5">
+    	<div class="col">
+        <h1 class="display-1">Générer votre qrcode gratuitement</h1>
+        <!-- <p>This is an example of header.<br>Place here your contents, or remove the file <code>/template/<span class="bg-danger text-white px-1">header.php</span></code>.</p> -->
+        <p><a href="#" class="btn btn-primary btn-lg shadow" role="button">Lire plus &raquo;</a></p>
+    </div>
+    </div>
+  </div>
+</div>
